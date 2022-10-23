0eedca84380ff451b54da5f21ee1852ce4cfe5f2
vuxmen <vutranvan.libra@gmail.com>
2022-10-23 16:26:03 +0700

    Routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World')
+import express, {Request, Response} from 'express'
+import cors from 'cors'
+import {routes} from './routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+  origin: ['http://localhost:3001']
+}))
+
+routes(app)
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('hello world')
+// })
+
+app.listen(8000, () => {
+  console.log('listen to port 8000')
+})
